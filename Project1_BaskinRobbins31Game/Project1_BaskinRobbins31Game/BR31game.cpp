4298020b26486aeb91854a45666e7b7d43bf4ab0
YoungWoong-Kim98 <woongene0518@gmail.com>
2023-02-16 17:48:30 +0900

    업데이트

--- Project1_BaskinRobbins31Game/Project1_BaskinRobbins31Game/BR31game.cpp
+++ Project1_BaskinRobbins31Game/Project1_BaskinRobbins31Game/BR31game.cpp
@@ -1,59 +1,37 @@
 #include<iostream>
 #include<cstdlib>
 using namespace std;
-
 int main() {
 	srand(time(NULL));
 	int count = 0;
 	int num = 0;
-	int i = num;
+	int a = 0;
+	string name;
 	while (count < 31) {
-		cout << "숫자를 입력해 주세요 : ";
-		cin >> num;
-		cout << "사용자가 입력한 숫자!\n";
-		if (count == 30) {
-			count = 31;
-			cout << count << endl;
-			cout << "게임 종료!! 컴퓨터의 승리 입니다!!";
-			break;
-		}
-		int j = 1;
-		if (num > 0 && num < 4) {
-			for (i = num, j = j + i; i > 0; i--) {
-				count++, j--;
-				cout << count << endl;
-				if (j == 1 && count < 27) {
-					cout << "컴퓨터가 부른 숫자!\n";
-					num = rand() % 3 + 2;
-					i = num;
-				}
-				if (j == 1 && count == 30) {
-					count = 31;
-					cout << "컴퓨터가 부른 숫자!\n";
-					cout << count << endl;
-					cout << "게임 종료!! 사용자의 승리 입니다!!";
-					break;
-				}
-				if (j == 1 && count == 29) {
-					cout << "컴퓨터가 부른 숫자!\n";
-					num++;
-					i = num;
-				}
-				if (j == 1 && count == 28) {
-					cout << "컴퓨터가 부른 숫자!\n";
-					num = num + 2;
-					i = num;
-				}
-				if (j == 1 && count == 27) {
-					cout << "컴퓨터가 부른 숫자!\n";
-					num = num + 3;
-					i = num;
-				}
+		if (num == 0 && a == 0) {
+			name = "컴퓨터";
+			a = 1;
+			cout << "숫자를 입력해 주세요 : ";
+			cin >> num;
+			if (num == 0 && num > 4) {
+				cout << "1~3사이의 숫자를 입력해 주세요.\n";
 			}
+			cout << "사용자가 입력한 숫자!\n";
 		}
-		else {
-			cout << "1~3사이의 숫자를 입력해 주세요.\n";
+		else if (num == 0 && a == 1) {
+			name = "사용자";
+			a = 0;
+			num = rand() % 3 + 1;
+			cout << "컴퓨터가 부른 숫자!\n";
 		}
-	}
- return 0;
+	    for (int i = num; i > 0; i--) {
+		count++, num--;
+		cout << count << endl;
+		    if (count == 31) {
+			    cout << "게임 종료!!" << name << "의 승리 입니다!!";
+				break;
+	     	}
+	    }
+    }
+	return 0;
 }
--- Project2_TAgame/Project2_TAgame/TAgame.cpp
+++ Project2_TAgame/Project2_TAgame/TAgame.cpp
@@ -4,13 +4,22 @@
 using namespace std;
 int main(void) {
 	string word;
-	string a;
-	clock_t startTime = clock();
-	clock_t endTime = clock();
-	while ((startTime / CLOCKS_PER_SEC) < 10) {
-		startTime;
+	string a,b;
+	int n = word.size();
+	a = word[0];
+	b = word[n - 1];
+	while (1) {
+		clock_t startTime = clock();
 		cin >> a;
+		if (a.compare(b) == 0)
+			cout << "s1 is equal to s2" << endl;
+
+		if (a.compare(c) == 0)
+			cout << "a is equal to c" << endl;
+		clock_t endTime = clock();
+		if ((endTime - startTime) / CLOCKS_PER_SEC > 10) {
+			cout << "е╦юс╬ф©Т";
+		}
 	}
-	cout << "е╦юс╬ф©Т";
 	return 0;
 }

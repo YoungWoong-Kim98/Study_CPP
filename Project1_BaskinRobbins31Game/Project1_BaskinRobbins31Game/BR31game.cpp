55a534087c4341fa352a7a693e8e3a8a908f6942
YoungWoong-Kim98 <124331681+YoungWoong-Kim98@users.noreply.github.com>
2023-02-24 15:42:02 +0900

    Update BR31game.cpp

--- Project1_BaskinRobbins31Game/Project1_BaskinRobbins31Game/BR31game.cpp
+++ Project1_BaskinRobbins31Game/Project1_BaskinRobbins31Game/BR31game.cpp
@@ -6,12 +6,13 @@ int main() {
 	int count = 0;
 	int num = 0;
 	// int a = 0;
-	string name = "사용자";
+	string name = "사용자"; 
 	while (count < 31) {
-		if (name == "사용자") { //while문 반복한번에 사용자, 컴퓨터 둘중 하나만 실행되게 하기위해
+		if (name == "사용자") { // while문 반복한번에 사용자, 컴퓨터 둘중 하나만 실행되게 하기위해
 			num = 0;
-			name = "컴퓨터"; //다음 반복때 컴퓨터 차례로 넘기기 위해 name변수에 컴퓨터를 입력
-			for (num; num < 1; num) {
+			name = "컴퓨터"; // 다음 반복때 컴퓨터 차례로 넘기기 위해 name변수에 컴퓨터를 입력
+			                 // 강사님께 확인전에는 변수a값을 1,0로 바꾸면서 차례변경하였으나 name으로 대체 
+			for (num; num < 1; num) { 
 				cout << "숫자를 입력해 주세요 : ";
 				cin >> num;
 				if (num < 0 || num > 4) {
@@ -22,11 +23,11 @@ int main() {
 			cout << "사용자가 입력한 숫자!\n";
 		}
 		else if (name == "컴퓨터") {
-			name = "사용자"; //다음 반복때 사용자 차례로 넘기기 위해 name변수에 사용자를 입력
+			name = "사용자"; // 다음 반복때 사용자 차례로 넘기기 위해 name변수에 사용자를 입력
 			num = rand() % 3 + 1;
 			cout << "컴퓨터가 부른 숫자!\n";
 		}
-	    for (int i = num; i > 0; i--) { // 원래 사용자, 컴퓨터 각각 하나씩 작성하였으나 하나로 수정
+	    for (int i = num; i > 0; i--) { // 강사님께 확인전에는 사용자, 컴퓨터 각각 하나씩 작성하였으나 하나로 수정
 		count++;
 		cout << count << endl;
 		    if (count == 31) {
